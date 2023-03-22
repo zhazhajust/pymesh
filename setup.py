b53e61753573e9326e074651c87a8d80ea3a1234
zhazhajust <251338258@qq.com>
2023-03-22 15:49:09 +0800

    pymesh

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pymesh",
+    name="pymesh3d",
     version="0.0.1",
     author="Cai Jie",
     author_email="jiecai@stu.pku.edu.cn",
