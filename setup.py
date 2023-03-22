ab49af4e9bca52f2ed7c8aa4f8156a1692e27995
Jie Cai <jiecai@stu.pku.edu.cn>
2023-03-22 14:47:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="mesh extract and render by python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    #url="https://github.com/251338258/sampleproject",
+    url="https://github.com/zhazhajust/pymesh.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
